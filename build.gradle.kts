380a7d47c832bbd2b3b8fa5a981deb47f3825bca
Jay Turner <jay.turner@kayenta.io>
2023-03-17 17:28:53 +0000

    Add android tools to buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        val kotlinVersion = "1.8.0"
+        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
+    }
+}
+
 plugins {
     kotlin("jvm") version "1.8.0"
     id("org.openstreetmap.josm").version("0.8.2")
--- src/main/kotlin/org/openstreetmap/josm/plugins/streetcompleteoverlay/StreetCompleteOverlayPlugin.kt
+++ src/main/kotlin/org/openstreetmap/josm/plugins/streetcompleteoverlay/StreetCompleteOverlayPlugin.kt
@@ -3,7 +3,8 @@ package org.openstreetmap.josm.plugins.streetcompleteoverlay
 import org.openstreetmap.josm.plugins.Plugin
 import org.openstreetmap.josm.plugins.PluginInformation
 import org.openstreetmap.josm.tools.Destroyable
+import de.westnordost.streetcomplete.StreetCompleteApplication
 
 class StreetCompleteOverlayPlugin(info: PluginInformation) : Plugin(info), Destroyable {
-
+    var application: StreetCompleteApplication = StreetCompleteApplication()
 }
